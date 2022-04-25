6899de04a6b7780130e257d46fa2e649b7ac8d45
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2022-04-25 22:57:53 +0200

    Added basic logic

--- js/app.js
+++ js/app.js
@@ -2,3 +2,30 @@ import { verbs } from '../data/verbs.js';
 
 console.log(verbs);
 const generateVerbsButton = document.querySelector('#generate-verbs-button');
+
+const returnVerbsLength = () => {
+	let counter = 0;
+	for (let verb in verbs) {
+		counter++;
+	}
+	return counter;
+};
+
+const randomiseVerbs = (number) => {
+	let arrayWithDuplicates = [];
+	let arrayOfRandomNumbers = [];
+
+	while (arrayOfRandomNumbers.length < 7) {
+		let randomNumber = Math.floor(Math.random() * number);
+		arrayWithDuplicates.push(randomNumber);
+		arrayOfRandomNumbers = [...new Set(arrayWithDuplicates)];
+	}
+	arrayOfRandomNumbers.forEach((number) => {
+		console.log(verbs[number].name);
+	});
+};
+
+generateVerbsButton.addEventListener('click', () => {
+	let verbsCount = returnVerbsLength();
+	randomiseVerbs(verbsCount);
+});
