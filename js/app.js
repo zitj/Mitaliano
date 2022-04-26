4a7bcb9181530e0719e9de235e4ff3710c3c02aa
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2022-04-27 00:01:31 +0200

    Restyled, reordered and added debounce

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 import { verbs } from '../data/verbs.js';
 
-const generateVerbsButton = document.querySelector('#generate-verbs-button');
 const wantedVerbsInput = document.querySelector('#wanted-verbs-input');
+const verbsContainer = document.querySelector('#verbs-container');
+const heading = verbsContainer.children[0];
+let randomVerbs = [];
+
+const debounce = (fn, delay) => {
+	let timeout;
+	return function (...args) {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			fn(...args);
+		}, delay);
+	};
+};
 
 const returnVerbsLength = () => {
 	let counter = 0;
@@ -14,22 +26,26 @@ const returnVerbsLength = () => {
 const showRandomisedVerbs = (totalNumberOfVerbs, wantedNumberOfVerbs) => {
 	let arrayWithDuplicates = [];
 	let arrayOfRandomNumbers = [];
-	let verbsPerWeek = wantedNumberOfVerbs * 7;
+	randomVerbs = [];
 
 	if (wantedNumberOfVerbs > totalNumberOfVerbs) {
-		let errorMessage = 'Wanted number of verbs must be smaller than total number of verbs!';
-		console.error(errorMessage);
+		let errorMessage = `Wanted number of verbs must be smaller than ${totalNumberOfVerbs}`;
+		heading.innerHTML = errorMessage;
 		return;
 	}
+
+	heading.innerHTML = 'Verbs to conjugate:';
 
 	while (arrayOfRandomNumbers.length < wantedNumberOfVerbs) {
 		let randomNumber = Math.floor(Math.random() * totalNumberOfVerbs);
 		arrayWithDuplicates.push(randomNumber);
 		arrayOfRandomNumbers = [...new Set(arrayWithDuplicates)];
 	}
+
 	arrayOfRandomNumbers.forEach((number) => {
-		console.log(verbs[number].name);
+		randomVerbs.push(verbs[number].name);
 	});
+	console.log(randomVerbs);
 };
 
 const randomiseVerbs = () => {
@@ -38,6 +54,13 @@ const randomiseVerbs = () => {
 	showRandomisedVerbs(verbsCount, wantedVerbsNumber);
 };
 
-generateVerbsButton.addEventListener('click', () => {
-	randomiseVerbs();
-});
+// generateVerbsButton.addEventListener('click', () => {
+// 	randomiseVerbs();
+// });
+
+wantedVerbsInput.addEventListener(
+	'keyup',
+	debounce((e) => {
+		randomiseVerbs();
+	}, 500)
+);
