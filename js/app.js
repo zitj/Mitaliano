702cc2d4b95c375a9cc11f7e12a84f93a79e564a
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2022-04-27 22:14:40 +0200

    Adding and removing classes on elements

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ import { verbs } from '../data/verbs.js';
 
 const wantedVerbsInput = document.querySelector('#wanted-verbs-input');
 const verbsContainer = document.querySelector('#verbs-container');
+const listOfVerbs = document.querySelector('#list-of-verbs');
 const heading = verbsContainer.children[0];
+
 let randomVerbs = [];
 
 const debounce = (fn, delay) => {
@@ -23,17 +25,29 @@ const returnVerbsLength = () => {
 	return counter;
 };
 
+const renderVerbs = () => {
+	let content = ``;
+	randomVerbs.forEach((verb) => {
+		content += `<p>${verb}</p>`;
+	});
+	listOfVerbs.innerHTML = content;
+	listOfVerbs.classList.add('active');
+};
+
 const showRandomisedVerbs = (totalNumberOfVerbs, wantedNumberOfVerbs) => {
 	let arrayWithDuplicates = [];
 	let arrayOfRandomNumbers = [];
 	randomVerbs = [];
 
 	if (wantedNumberOfVerbs > totalNumberOfVerbs) {
-		let errorMessage = `Wanted number of verbs must be smaller than ${totalNumberOfVerbs}`;
+		let errorMessage = `Number must be smaller than ${totalNumberOfVerbs}`;
 		heading.innerHTML = errorMessage;
+		listOfVerbs.innerHTML = '';
+		heading.classList.add('error');
+		listOfVerbs.classList.remove('active');
 		return;
 	}
-
+	heading.classList.remove('error');
 	heading.innerHTML = 'Verbs to conjugate:';
 
 	while (arrayOfRandomNumbers.length < wantedNumberOfVerbs) {
@@ -45,6 +59,7 @@ const showRandomisedVerbs = (totalNumberOfVerbs, wantedNumberOfVerbs) => {
 	arrayOfRandomNumbers.forEach((number) => {
 		randomVerbs.push(verbs[number].name);
 	});
+	renderVerbs();
 };
 
 const randomiseVerbs = () => {
@@ -57,7 +72,9 @@ wantedVerbsInput.addEventListener(
 	'keyup',
 	debounce((e) => {
 		if (e.target.value === '') {
-			heading.innerHTML = ' ';
+			heading.innerHTML = '';
+			listOfVerbs.innerHTML = '';
+			listOfVerbs.classList.remove('active');
 			return;
 		}
 		randomiseVerbs();
