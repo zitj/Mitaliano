fc836ce2283fa9e4fde93a5e1eb1ef2355948075
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2023-04-22 09:54:03 +0200

    Added file with new italian words I'm learning

---
+++ data/words.js
@@ -0,0 +1,38 @@
+export const words = {
+	0: {
+		name: 'la raccolta',
+		translation: 'zbirka',
+		context: 'Che fa parte della raccolta "Sessanta racconti", pubblicato nel 1958.',
+		source: 'I sette messaggeri',
+	},
+	1: {
+		name: 'il regno',
+		translation: 'kraljevstvo',
+		context: "Sono partito quando avevo poco più di trent'anni per esplorare il regno del mio padre.",
+		source: 'I sette messaggeri',
+	},
+	2: {
+		name: 'i confini',
+		translation: 'granice',
+		context: 'Pensavo di raggiungere i confini in poche settimane, invece sono passati più di otto anni.',
+		source: 'I sette messaggeri',
+	},
+	3: {
+		name: 'la bussola',
+		translation: 'kompas',
+		context: 'A volte penso che la bussola sia impazzita...',
+		source: 'I sette messaggeri',
+	},
+	4: {
+		name: 'impazzita',
+		translation: 'poludela',
+		context: 'A volte penso che la bussola sia impazzita...',
+		source: 'I sette messaggeri',
+	},
+	5: {
+		name: 'il cerchio',
+		translation: 'krug',
+		context: 'Continuiamo a girare in cerchio.',
+		source: 'I sette messaggeri',
+	},
+};
