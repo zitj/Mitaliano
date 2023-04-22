8adcc21d7c3877b8cb526e3c81b91cc2680c5ed2
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2023-04-22 11:10:41 +0200

    Added new words

--- data/words.js
+++ data/words.js
@@ -7,7 +7,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'la', word: 'raccolta' },
 		plural: { article: 'le', word: 'raccolte' },
-		gender: 'F',
+		gender: 'F', //next
 	},
 	1: {
 		word: 'regno',
@@ -17,7 +17,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'il', word: 'regno' },
 		plural: { article: 'i', word: 'regni' },
-		gender: 'M',
+		gender: 'M', //next
 	},
 	2: {
 		word: 'confini',
@@ -27,7 +27,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'il', word: 'confine' },
 		plural: { article: 'i', word: 'confini' },
-		gender: 'M',
+		gender: 'M', //next
 	},
 	3: {
 		word: 'bussola',
@@ -37,7 +37,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'la', word: 'bussola' },
 		plural: { article: 'le', word: 'bussole' },
-		gender: 'F',
+		gender: 'F', //next
 	},
 	4: {
 		word: 'impazzita',
@@ -47,7 +47,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '/', word: '/' },
 		plural: { article: '/', word: '/' },
-		gender: '/',
+		gender: '/', //next
 	},
 	5: {
 		word: 'cerchio',
@@ -57,7 +57,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'il', word: 'cerchio' },
 		plural: { article: 'i', word: 'cherchi' },
-		gender: 'M',
+		gender: 'M', //next
 	},
 	6: {
 		word: 'cavalieri',
@@ -67,7 +67,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'il', word: 'cavaliere' },
 		plural: { article: 'i', word: 'cavalieri' },
-		gender: 'maschile',
+		gender: 'maschile', //next
 	},
 	7: {
 		word: 'Mi rendo conto',
@@ -77,7 +77,7 @@ export const words = {
 		type: 'phrase',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	8: {
 		word: 'tenacia',
@@ -87,7 +87,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'la', word: 'tenacia' },
 		plural: { article: 'le', word: 'tenacie' },
-		gender: 'F',
+		gender: 'F', //next
 	},
 	9: {
 		word: 'devozione',
@@ -97,7 +97,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'il', word: 'devozione' },
 		plural: { article: 'i', word: 'devozioni' },
-		gender: 'M',
+		gender: 'M', //next
 	},
 	10: {
 		word: 'sella',
@@ -108,7 +108,7 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'la', word: 'sella' },
 		plural: { article: 'le', word: 'selle' },
-		gender: 'F',
+		gender: 'F', //next
 	},
 	11: {
 		word: 'Mi sono accorto',
@@ -119,7 +119,7 @@ export const words = {
 		type: 'phrase',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	12: {
 		word: 'bastava',
@@ -130,7 +130,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	13: {
 		word: 'impiegati',
@@ -141,7 +141,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	14: {
 		word: 'deboli',
@@ -151,7 +151,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	15: {
 		word: 'sgualcite',
@@ -161,7 +161,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	16: {
 		word: 'macchiate',
@@ -171,7 +171,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	17: {
 		word: 'trascorse',
@@ -181,7 +181,7 @@ export const words = {
 		type: 'adjective',
 		singular: { article: '', word: '' },
 		plural: { article: '', word: '' },
-		gender: '/',
+		gender: '/', //next
 	},
 	18: {
 		word: 'scoraggiamento',
@@ -191,6 +191,106 @@ export const words = {
 		type: 'noun',
 		singular: { article: 'lo', word: 'scoraggiamento' },
 		plural: { article: 'gli', word: 'scoraggiamenti' },
-		gender: 'maschile',
+		gender: 'M', //next
+	},
+	19: {
+		word: 'comportamento',
+		translation: 'ponašanje',
+		context: 'Il suo comportamento e cativo',
+		source: 'unknown',
+		type: 'noun',
+		singular: { article: 'il', word: 'comportamento' },
+		plural: { article: 'i', word: 'comportamenti' },
+		gender: 'M', //next
+	},
+	20: {
+		word: 'partenza',
+		translation: 'odlazak',
+		context: 'Erano passati quattro anni dalla mia partenza.',
+		source: 'I sette messaggeri',
+		type: 'noun',
+		singular: { article: 'la', word: 'partenza' },
+		plural: { article: 'le', word: 'partenze' },
+		gender: 'F', //next
+	},
+	21: {
+		word: 'vaghi',
+		translation: 'nejasni',
+		context: 'Il ricordo della mia città, della mia casa, di mio padre diventavano sempre più vaghi.',
+		source: 'I sette messaggeri',
+		type: 'adjective',
+		singular: { article: '', word: '' },
+		plural: { article: '', word: '' },
+		gender: '/', //next
+	},
+	22: {
+		word: 'ingiallite',
+		translation: 'požutele',
+		context: 'Mi portavano lettere ingiallite dal tempo.',
+		source: 'I sette messaggeri',
+		type: 'adjective',
+		singular: { article: '', word: '' },
+		plural: { article: '', word: '' },
+		gender: '/', //next
+	},
+	23: {
+		word: 'insoliti',
+		translation: 'požutele',
+		context: 'Modi da dire insoliti e sentimenti che non riuscivo più a capire.',
+		source: 'I sette messaggeri',
+		type: 'adjective',
+		singular: { article: '', word: '' },
+		plural: { article: '', word: '' },
+		gender: '/', //next
+	},
+	24: {
+		word: 'stravolto',
+		translation: 'uvrnuto',
+		context: 'Riesce ancora a sorridere, anche se stravolto dalla fatica.',
+		source: 'I sette messaggeri',
+		type: 'adjective',
+		singular: { article: '', word: '' },
+		plural: { article: '', word: '' },
+		gender: '/', //next
+	},
+	25: {
+		word: 'praterie',
+		translation: 'travnjaci',
+		context: 'Non ha fatto altro che correre, attraverso praterie, boschi e deserti.',
+		source: 'I sette messaggeri',
+		type: 'noun',
+		singular: { article: 'il', word: 'prato' },
+		plural: { article: 'le', word: 'praterie' },
+		gender: 'M', //next
+	},
+	26: {
+		word: 'boschi',
+		translation: 'šume',
+		context: 'Non ha fatto altro che correre, attraverso praterie, boschi e deserti.',
+		source: 'I sette messaggeri',
+		type: 'noun',
+		singular: { article: 'il', word: 'bosco' },
+		plural: { article: 'i', word: 'boschi' },
+		gender: 'M', //next
+	},
+	27: {
+		word: 'pietra',
+		translation: 'kamen',
+		context: 'Hanno costruito alti palazzi di pietra.',
+		source: 'I sette messaggeri',
+		type: 'noun',
+		singular: { article: 'la', word: 'pietra' },
+		plural: { article: 'le', word: 'pietre' },
+		gender: 'F', //next
+	},
+	28: {
+		word: 'querce',
+		translation: 'hrastovi',
+		context: 'Le querce sotto cui andavo a giocare da bambino.',
+		source: 'I sette messaggeri',
+		type: 'noun',
+		singular: { article: 'la', word: 'quercia' },
+		plural: { article: 'le', word: 'querce' },
+		gender: 'F', //next
 	},
 };
