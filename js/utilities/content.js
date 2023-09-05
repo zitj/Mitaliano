c0a4ea8cb0e8c74151bfc74b651a0e7b5a6748e0
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2023-09-05 14:28:25 +0200

    Code refactored in content.js

--- js/utilities/content.js
+++ js/utilities/content.js
@@ -1,52 +1,63 @@
 import { TEXT } from '../constants.js';
 
-const createContent = (modifiers, element) => {
+const returnContentForVerbsSection = (modifiers) => {
+	let word = modifiers.contentObj.element;
+	return `
+	<a class="verb-link" href='https://www.italian-verbs.com/italian-verbs/conjugation.php?parola=${word}'  target="_blank">${word}</a>
+`;
+};
+
+const returnContentForWordSection = (modifiers) => {
+	let element = modifiers.contentObj.element;
+	let context = element.context.replace(element.word, `<span class="context-word">${element.word}</span>`);
+	let button = `<button id="next-btn">&rarr;</button>`;
+	return `
+		<div class="card">
+			<img src="./img/filter.png" alt="filter" class="filter-icon">
+			<h2>${element.word}</h2>
+			<p class="context">${context}</p>
+			<button class="translation-button" id=${element.id}>${TEXT.SHOW_TRANSLATION}</button>
+			<p class="translation-word hide" >${element.translation}</p>
+			${modifiers.contentObj.totalNumberOfElements > 1 ? button : ''}
+		</div>
+	`;
+};
+
+const returnContentForGameWord = (word) => {
+	let splittedClass = '';
+	if (word.length > 12) splittedClass = 'splitted';
+	return `
+			<div class="game-word ${splittedClass}">${splitString(word)}</div>
+			`;
+};
+
+const returnContentForGameSection = (modifiers) => {
+	let element = modifiers.contentObj.element;
+	let content = modifiers.contentObj.content;
+	if (!modifiers.sectionModifier.arrayShuffled) {
+		content += returnContentForGameWord(element.word);
+	} else {
+		let translatedWord = element.translation;
+		let synonyms = translatedWord.split(',');
+		if (synonyms.length > 1) {
+			const randomIndex = Math.floor(Math.random() * synonyms.length);
+			translatedWord = synonyms[randomIndex];
+		}
+		content += returnContentForGameWord(translatedWord);
+	}
+	return content;
+};
+
+const createContent = (modifiers) => {
 	let content = ``;
 	let type = modifiers.sectionModifier.type;
 	let totalNumberOfElements = Object.keys(modifiers.filterModifier.wordsObj).length;
+	modifiers.contentObj.totalNumberOfElements = totalNumberOfElements;
+	modifiers.contentObj.content = content;
 
-	if (type === TEXT.VERBS) {
-		content = `
-            <a class="verb-link" href='https://www.italian-verbs.com/italian-verbs/conjugation.php?parola=${element}'  target="_blank">${element}</a>
-        `;
-	}
-	if (type === TEXT.WORDS) {
-		let context = element.context.replace(element.word, `<span class="context-word">${element.word}</span>`);
-		let button = `<button id="next-btn">&rarr;</button>`;
-
-		content += `
-            <div class="card">
-				<img src="./img/filter.png" alt="filter" class="filter-icon">
-                <h2>${element.word}</h2>
-                <p class="context">${context}</p>
-                <button class="translation-button" id=${element.id}>${TEXT.SHOW_TRANSLATION}</button>
-				<p class="translation-word hide" >${element.translation}</p>
-				${totalNumberOfElements > 1 ? button : ''}
-            </div>
-        `;
-	}
-
-	if (type === TEXT.GAME) {
-		if (!modifiers.sectionModifier.arrayShuffled) {
-			let splittedClass = '';
-			if (element.word.length > 12) splittedClass = 'splitted';
-			content += `
-				<div class="game-word ${splittedClass}">${splitString(element.word)}</div>
-				`;
-		} else {
-			let translatedWord = element.translation;
-			let multipleWords = translatedWord.split(',');
-			if (multipleWords.length > 1) {
-				const randomIndex = Math.floor(Math.random() * multipleWords.length);
-				translatedWord = multipleWords[randomIndex];
-			}
-			let splittedClass = '';
-			if (translatedWord.length > 12) splittedClass = 'splitted';
-			content += `
-				<div class="game-word ${splittedClass}">${splitString(translatedWord)}</div>
-				`;
-		}
-	}
+	if (type === TEXT.VERBS) content = returnContentForVerbsSection(modifiers);
+	if (type === TEXT.WORDS) content += returnContentForWordSection(modifiers);
+	if (type === TEXT.GAME) content = returnContentForGameSection(modifiers);
 
 	return content;
 };
@@ -70,8 +81,10 @@ const shuffleArray = (array) => {
 
 const returnContentForEachElement = (modifiers) => {
 	let content = ``;
+	modifiers.contentObj = { element: '', content };
 	modifiers.sectionModifier.randomElements.forEach((element) => {
-		content += createContent(modifiers, element);
+		modifiers.contentObj.element = element;
+		content += createContent(modifiers);
 	});
 	return content;
 };
